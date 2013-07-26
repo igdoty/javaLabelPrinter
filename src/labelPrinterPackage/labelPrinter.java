52d02838fedb10edaff6780a7855f2d3ab217edf
igdoty <igdoty@gmail.com>
2013-07-25 17:06:07 -0700

    Added GUI

--- src/labelPrinterPackage/labelPrinter.java
+++ src/labelPrinterPackage/labelPrinter.java
@@ -1,7 +1,8 @@
 package labelPrinterPackage;
 
-import java.io.Console;
-import java.util.Scanner;
+import java.io.*;
+import java.awt.*;
+import java.util.*;
 
 import javax.print.Doc;
 import javax.print.DocFlavor;
@@ -16,37 +17,85 @@ import javax.print.event.PrintJobEvent;
 import javax.print.event.PrintJobListener;
 
 
+
+import javax.swing.*;
+
+import bsh.util.GUIConsoleInterface;
+import bsh.util.JConsole;
+
+
 public class labelPrinter {
 
 	private static String url = "http://admin.touchofmodern.local:3000/shipments/scan_label";
+	private static Queue<String> queue = new LinkedList<String>();
 
 	public static void main(String[] args) throws Exception{
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter a username: ");
-		String username = scan.nextLine();
-		System.out.print("Enter a password: ");
-		String password = scan.nextLine();
+		
+		JTextField unField = new JTextField();
+		JTextField pwField = new JTextField();
+		JTextField textField = new JTextField();
+		
+		Object[] message = {
+				"Login Credentials for ToMo Admin",
+			    "Username:", unField,
+			    "Password:", pwField,
+		};
+		JTextField snField = new JTextField();
+		
+		Object[] labelMessage = {
+			"Shipping number",
+			snField
+		};
+		
+		Object[] options = {"OK"};
+		JOptionPane.showOptionDialog(null, message, "Login", JOptionPane.PLAIN_MESSAGE, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+		
+		String username = unField.getText();
+		String password = pwField.getText();
+		
+		
+		
+//		Scanner scan = new Scanner(System.in);
+//		System.out.print("Enter a username: ");
+//		String username = scan.nextLine();
+//		System.out.print("Enter a password: ");
+//		String password = scan.nextLine();
+		String shippingNumber;
 		while(true){
-			System.out.println("Enter shipping numbers to print labels, or an empty line to close the application");
-			System.out.print("Enter a shipping number: ");
-			String shippingNumber = scan.nextLine();
+//			System.out.println("Enter shipping numbers to print labels, or an empty line to close the application");
+//			System.out.print("Enter a shipping number: ");
+//			String shippingNumber = scan.nextLine();
+			
+			/*Get Shipping number*/
+			JOptionPane.showOptionDialog(null, labelMessage, "Enter Shipping Number", JOptionPane.PLAIN_MESSAGE, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+			shippingNumber = snField.getText();
 			if(shippingNumber == ""){
-				scan.close();
+//				scan.close();
 				break;
 			}
 			httpPost post = new httpPost(username,password, url,shippingNumber);
 			if(post.wasRedirected()){
-				System.out.println("Request was redirected, try reenetering username and password");
-				System.out.print("Enter a username: ");
-				username = scan.nextLine();
-				System.out.print("Enter a password: ");
-				password = scan.nextLine();
+				message[0] = "Request redirected, retry login";
+				
+				JOptionPane.showOptionDialog(null, message, "Login", JOptionPane.PLAIN_MESSAGE, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+				username = unField.getText();
+				password = pwField.getText();
+				
+//				System.out.println("Request was redirected, try reenetering username and password");
+//				System.out.print("Enter a username: ");
+//				username = JOptionPane.showInputDialog(null,"Page was redirected, try reentering username and password");
+//				username = scan.nextLine();
+//				System.out.print("Enter a password: ");
+//				password = scan.nextLine();
+				
 			}else if(post.labelFailed()){
-				System.out.println("Failed to generate label, try reentering shipping number");
+//				System.out.println("Failed to generate label, try reentering shipping number");
+				JOptionPane.showMessageDialog(null, "Failed to generate label, try reentering shipping number");
+				
 			}else{
 				String command = post.getDecoded();
 				System.out.println(command);
-//				printLabel(command);
+				printLabel(command);
 			}
 
 		}
@@ -87,7 +136,7 @@ public class labelPrinter {
 		} 
 		return true;
 	}
-
+	
 }
 
 
